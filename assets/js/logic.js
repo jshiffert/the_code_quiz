25ca758eda9bb5789dee2f8125d32dcebc1844aa
jshiffert <jshiffert@oki.org>
2023-05-25 11:33:54 -0400

    added js for time and start, added placeholders

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,29 +1,29 @@
 // variables to keep track of quiz state
 var currentQuestionIndex = 0;
 //time left value here
-// var time = ;
+var time = 60;
 var timerId;
 
 // variables to reference DOM elements
-var questionsEl = document.getElementById('');
-var timerEl = document.getElementById('');
-var choicesEl = document.getElementById('');
-var submitBtn = document.getElementById('');
-var startBtn = document.getElementById('');
-var initialsEl = document.getElementById('');
-var feedbackEl = document.getElementById('');
+var questionsEl = document.getElementById('questions');
+var timerEl = document.getElementById('time');
+var choicesEl = document.getElementById('choices');
+var submitBtn = document.getElementById('submit');
+var startBtn = document.getElementById('start');
+var initialsEl = document.getElementById('initials');
+var feedbackEl = document.getElementById('feedback');
 
 
 function startQuiz() {
   // hide start screen
-  var startScreenEl = document.getElementById('');
-  startScreenEl.setAttribute('class', '');
+  var startScreenEl = document.getElementById('start-screen');
+  startScreenEl.setAttribute('class', 'hide');
 
   // un-hide questions section
-  questionsEl.removeAttribute('');
+  questionsEl.removeAttribute('class', 'hide');
 
   // start timer
-  timerId = setInterval(clockTick, 1000);
+  timerId = setInterval(clockTick(), 1000);
 
   // show starting time
   timerEl.textContent = time;
@@ -110,9 +110,9 @@ function quizEnd() {
 }
 
 function clockTick() {
-  // update time
-  // decrement the variable we are using to track time
-  timerEl.textContent = ; // update out time
+  timerEl.textContent = time;// update time
+  time--; // decrement the variable we are using to track time
+  timerEl.textContent = time; // update out time
 
   // check if user ran out of time
   if (time <= 0) {
